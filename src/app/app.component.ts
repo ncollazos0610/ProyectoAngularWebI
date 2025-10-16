ad4b1bd9d9ab01d0c4fb9fbf5523e8aa674262d6
jdiazt-troyano <jdiazt@unimayor.edu.co>
2025-10-16 17:08:39 -0500

    Cambios en img, en html y creacion de routing de app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <app-home></app-home>
+  `
 })
 export class AppComponent {
-  title = 'MiProjecto';
+  title = 'MiProjecto'; 
 }

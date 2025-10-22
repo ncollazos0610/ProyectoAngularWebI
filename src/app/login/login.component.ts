a2190a76f954d4ca3feb98b2403750fb61a42059
Nicolas <ncollazos@unimayor.edu.co>
2025-10-22 14:48:09 -0500

    Cambios en Routes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   templateUrl: './login.component.html',
+  imports: [CommonModule, RouterModule],
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {}
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-registro',               // Nombre del componente (debe coincidir con lo que usarás en HTML)
-  templateUrl: './registro.component.html', // Ruta del archivo HTML
+  selector: 'app-registro',    
+  standalone: true,           // Nombre del componente (debe coincidir con lo que usarás en HTML)
+  templateUrl: './registro.component.html', 
+  imports: [CommonModule, RouterModule],// Ruta del archivo HTML
   styleUrls: ['./registro.component.css']   // Ruta del archivo CSS
 })
 export class RegistroComponent {
